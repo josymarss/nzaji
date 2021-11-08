f66046f83f696cfb74f6d35d11787603879830d1
josymarss <sylvajosymar@gmail.com>
2021-11-08 23:40:51 +0100

    Adding structure

---
+++ nzaji/target/rls/debug/deps/nzaji-703b47238071324f.d
@@ -0,0 +1,5 @@
+c:\Users\Josymar\Documents\Projects\github\Nzaji project\nzaji\nzaji\target\rls\debug\deps\nzaji-703b47238071324f.rmeta: src\main.rs
+
+c:\Users\Josymar\Documents\Projects\github\Nzaji project\nzaji\nzaji\target\rls\debug\deps\nzaji-703b47238071324f.d: src\main.rs
+
+src\main.rs:
---
+++ nzaji/target/rls/debug/deps/nzaji-7b1310fa4dd190c7.d
@@ -0,0 +1,5 @@
+c:\Users\Josymar\Documents\Projects\github\Nzaji project\nzaji\nzaji\target\rls\debug\deps\nzaji-7b1310fa4dd190c7.rmeta: src\main.rs
+
+c:\Users\Josymar\Documents\Projects\github\Nzaji project\nzaji\nzaji\target\rls\debug\deps\nzaji-7b1310fa4dd190c7.d: src\main.rs
+
+src\main.rs:
